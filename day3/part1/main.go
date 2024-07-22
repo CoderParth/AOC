53ee4615fc993226485a3383ab217d5a4d2ed282
CoderParth <ghantigalli@gmail.com>
2024-07-22 16:47:54 +1000

    draft: initial draft for day3-part1

---
+++ day3/part1/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+// 467..114..
+// ...*......
+// ..35..633.
+// ......#...
+// 617*......
+// .....+.58.
+// ..592.....
+// ......755.
+// ...$.*....
+// .664.598..
+//
+
+// In this schematic, two numbers are not part numbers
+// because they are not adjacent to a symbol: 114 (top right)
+// and 58 (middle right). Every other number is adjacent to a
+// symbol and so is a part number; their sum is 4361.
+//
+// Of course, the actual engine schematic is much larger.
+// What is the sum of all of the part numbers in the engine schematic?
+
+func main() {
+	allPartNums := findPartNums()
+	fmt.Printf("Part Nums: %v\n", allPartNums)
+	totalSumOfPartNums := findTotalSum(allPartNums)
+	fmt.Printf("Total sum of part nums: %v\n", totalSumOfPartNums)
+}
