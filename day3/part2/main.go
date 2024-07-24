75a2b6555c70014851734d6bd2e40f14a2087884
CoderParth <ghantigalli@gmail.com>
2024-07-24 22:21:21 +1000

    initial: day3-part2

---
+++ day3/part2/main.go
@@ -0,0 +1,28 @@
+package main
+
+// 467..114..
+// ...*......
+// ..35..633.
+// ......#...
+// 617*......
+// .....+.58.
+// ..592.....
+// ......755.
+// ...$.*....
+// .664.598..
+
+// A gear is any * symbol that is adjacent
+// to exactly two part numbers. Its gear ratio
+// is the result of multiplying those two numbers together.
+//
+//
+//In this schematic, there are two gears. The first is in
+//the top left; it has part numbers 467 and 35, so its gear ratio
+//is 16345. The second gear is in the lower right; its gear ratio
+// is 451490. (The * adjacent to 617 is not a gear because it
+// is only adjacent to one part number.) Adding up all of the
+// gear ratios produces 467835.
+
+// What is the sum of all of the gear ratios in your engine schematic?
+func main() {
+}
