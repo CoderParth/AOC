2f6ffee6051488dfed2681dd4c5452e4210ef385
CoderParth <ghantigalli@gmail.com>
2024-07-27 11:10:36 +1000

    initial: setup for day5-part1

---
+++ day5/main.go
@@ -0,0 +1,54 @@
+package main
+
+// seeds: 79 14 55 13
+//
+// seed-to-soil map:
+// 50 98 2
+// 52 50 48
+//
+// soil-to-fertilizer map:
+// 0 15 37
+// 37 52 2
+// 39 0 15
+//
+// fertilizer-to-water map:
+// 49 53 8
+// 0 11 42
+// 42 0 7
+// 57 7 4
+//
+// water-to-light map:
+// 88 18 7
+// 18 25 70
+//
+// light-to-temperature map:
+// 45 77 23
+// 81 45 19
+// 68 64 13
+//
+// temperature-to-humidity map:
+// 0 69 1
+// 1 0 69
+//
+// humidity-to-location map:
+// 60 56 37
+// 56 93 4
+//
+// The gardener and his team want to get started as soon as possible,
+// so they'd like to know the closest location that needs a seed.
+// Using these maps, find the lowest location number that corresponds
+// to any of the initial seeds. To do this, you'll need to convert
+// each seed number through other categories until you can find its
+// corresponding location number. In this example, the corresponding
+// types are:
+//
+// Seed 79, soil 81, fertilizer 81, water 81, light 74, temperature 78, humidity 78, location 82.
+// Seed 14, soil 14, fertilizer 53, water 49, light 42, temperature 42, humidity 43, location 43.
+// Seed 55, soil 57, fertilizer 57, water 53, light 46, temperature 82, humidity 82, location 86.
+// Seed 13, soil 13, fertilizer 52, water 41, light 34, temperature 34, humidity 35, location 35.
+// So, the lowest location number in this example is 35.
+//
+// What is the lowest location number that corresponds to any of the initial seed numbers?
+
+func main() {
+}
