f182cc3b94b0589a0bea14061cb448f2242e7c8d
CoderParth <ghantigalli@gmail.com>
2024-07-27 12:53:19 +1000

    initial: setup fro 2022-part1

---
+++ 2024/day1/part1/go.mod
@@ -0,0 +1,3 @@
+module github.com/coderparth/aoc-2022
+
+go 1.22.3
---
+++ 2024/day1/part1/main.go
@@ -0,0 +1,27 @@
+package main
+
+// 1000
+// 2000
+// 3000
+//
+// 4000
+//
+// 5000
+// 6000
+//
+// 7000
+// 8000
+// 9000
+//
+// 10000
+//
+// In case the Elves get hungry and need extra snacks,
+// they need to know which Elf to ask: they'd like to
+// know how many Calories are being carried by the Elf
+// carrying the most Calories. In the example above,
+// this is 24000 (carried by the fourth Elf).
+//
+// Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
+
+func main() {
+}
