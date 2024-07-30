5bd0ac2df52cf958fefa7ed568dbad2126bd261a
CoderParth <ghantigalli@gmail.com>
2024-07-30 17:03:26 +1000

    initial: setup for 2018/day1/part1

---
+++ 2018/day1/part1/main.go
@@ -0,0 +1,13 @@
+package main
+
+// other example situations:
+//
+// +1, +1, +1 results in  3
+// +1, +1, -2 results in  0
+// -1, -2, -3 results in -6
+// Starting with a frequency of zero,
+// what is the resulting frequency after
+// all of the changes in frequency have been applied?
+
+func main() {
+}
