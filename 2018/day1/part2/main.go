eacfe120c86a01e84983754c2c1537ecfaa75e43
CoderParth <ghantigalli@gmail.com>
2024-07-30 23:29:13 +1000

    update: solution of 2018/day1/part2

--- 2018/day1/part2/main.go
+++ 2018/day1/part2/main.go
@@ -9,33 +9,48 @@ import (
 )
 
 // other example situations:
-//
-// +1, +1, +1 results in  3
-// +1, +1, -2 results in  0
-// -1, -2, -3 results in -6
-// Starting with a frequency of zero,
-// what is the resulting frequency after
-// all of the changes in frequency have been applied?
+// // +1, -1 first reaches 0 twice.
+// +3, +3, +4, -2, -4 first reaches 10 twice.
+// -6, +3, +8, +5, -6 first reaches 5 twice.
+// +7, +7, -2, -7, -4 first reaches 14 twice.
+// What is the first frequency your device reaches twice?
 
+// Start at 0
+// Apply +1 → frequency is 1
+// Apply -2 → frequency is -1
+// Apply +3 → frequency is 2
+// Apply +1 → frequency is 3
+// Repeat from the beginning
+// Apply +1 again → frequency is 4
+// Apply -2 → frequency is 2 (which we have seen before)
+// The first frequency reached twice is 2.
 func main() {
 	resultingFrequency := calcResultingFreq()
 	fmt.Printf("Resulting Freq: %v \n", resultingFrequency)
 }
 
 func calcResultingFreq() int {
+	mp := make(map[int]int)
 	total := 0
+	mp[0] = 0
 	fileScanner := createFileScanner()
-	for fileScanner.Scan() {
-		currLine := fileScanner.Text()
-		n := len(currLine)
-		currNum, sign := findNumAndSign(currLine, n)
-		if sign == "+" {
-			total += currNum
-		} else {
-			total -= currNum
+	for {
+		for fileScanner.Scan() {
+			currLine := fileScanner.Text()
+			n := len(currLine)
+			currNum, sign := findNumAndSign(currLine, n)
+			if sign == "+" {
+				total += currNum
+			} else {
+				total -= currNum
+			}
+			if _, ok := mp[total]; ok {
+				return total
+			}
+			mp[total] = 0
 		}
+		fileScanner = createFileScanner()
 	}
-	return total
 }
 
 func findNumAndSign(currLine string, n int) (int, string) {
