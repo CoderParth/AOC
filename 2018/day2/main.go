6f470d05a7d3140d73ad6a7c6b2369a64ef2151b
CoderParth <ghantigalli@gmail.com>
2024-08-01 14:30:41 +1000

    draft: solution of 2018/day2/part1

--- 2018/day2/main.go
+++ 2018/day2/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
 )
@@ -22,11 +23,49 @@ import (
 //
 // What is the checksum for your list of box IDs?
 func main() {
-	input := readInputAndCollectStrings()
+	totalTwos, totalThrees := readInputAndCollectStrings()
+	checkSum := totalTwos * totalThrees
+	fmt.Printf("check sum: %v \n", checkSum)
 }
 
-func readInputAndCollectStrings() []string {
+func readInputAndCollectStrings() (int, int) {
 	fileScanner := createFileScanner()
+	totalTwos, totalThrees := 0, 0
+	for fileScanner.Scan() {
+		currLine := fileScanner.Text()
+		n := len(currLine)
+		fmt.Printf("curr line: %v \n", currLine)
+		isTwoPresent, isThreePresent := findTwosAndThrees(currLine, n)
+		fmt.Printf("twos: %v \n", isTwoPresent)
+		fmt.Printf("threes: %v \n", isThreePresent)
+		if isTwoPresent {
+			totalTwos++
+		}
+		if isThreePresent {
+			totalThrees++
+		}
+	}
+	return totalTwos, totalThrees
+}
+
+func findTwosAndThrees(currLine string, n int) (bool, bool) {
+	mp := make(map[string]int)
+	twos, threes := false, false
+	for i := 0; i < n; i++ {
+		currChar := string(currLine[i])
+		mp[currChar]++
+	}
+
+	for i := 0; i < n; i++ {
+		currChar := string(currLine[i])
+		if !twos && mp[currChar] == 2 {
+			twos = true
+		}
+		if !threes && mp[currChar] == 3 {
+			threes = true
+		}
+	}
+	return twos, threes
 }
 
 func createFileScanner() *bufio.Scanner {
