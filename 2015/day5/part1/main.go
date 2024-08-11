8e5766807941c58f752a534462015f177aee99c8
CoderParth <ghantigalli@gmail.com>
2024-08-11 14:35:28 +1000

    2015/day5/part1

--- 2015/day5/part1/main.go
+++ 2015/day5/part1/main.go
@@ -36,7 +36,75 @@ func main() {
 }
 
 func findNiceStrings() int {
+	total := 0
 	fileScanner := createFileScanner()
+	for fileScanner.Scan() {
+		currLine := fileScanner.Text()
+		n := len(currLine)
+		if isStringNice(currLine, n) {
+			total++
+		}
+	}
+	return total
+}
+
+func isStringNice(line string, n int) bool {
+	containsThreeVowels := checkIfStrHasThreeVowels(line, n)
+	letterAppearsTwice := checkIfLetterAppearsTwice(line, n)
+	doesNotContain := doesNotContainSpecificStrings(line, n)
+	if containsThreeVowels && letterAppearsTwice && doesNotContain {
+		return true
+	}
+	return false
+}
+
+func doesNotContainSpecificStrings(line string, n int) bool {
+	mp := map[string]int{
+		"ab": 0,
+		"cd": 0,
+		"pq": 0,
+		"xy": 0,
+	}
+	for i := 0; i < n-1; i++ {
+		curr := ""
+		curr += string(line[i]) + string(line[i+1])
+		if _, ok := mp[curr]; ok {
+			return false
+		}
+	}
+	return true
+}
+
+func checkIfLetterAppearsTwice(line string, n int) bool {
+	curr := string(line[0])
+	for i := 1; i < n; i++ {
+		if string(line[i]) == curr {
+			return true
+		}
+		curr = string(line[i])
+	}
+	return false
+}
+
+func checkIfStrHasThreeVowels(line string, n int) bool {
+	mp := map[string]int{
+		"a": 0,
+		"e": 0,
+		"i": 0,
+		"o": 0,
+		"u": 0,
+	}
+	for i := 0; i < n; i++ {
+		curr := string(line[i])
+		if _, ok := mp[curr]; ok {
+			mp[curr]++
+		}
+	}
+	numOfVowels := 0
+	for _, v := range mp {
+		numOfVowels += v
+	}
+	return numOfVowels >= 3
 }
 
 func createFileScanner() *bufio.Scanner {
