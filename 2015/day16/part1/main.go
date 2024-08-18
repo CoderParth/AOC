44c9d9491a6746d3be181c12d10cad1a4468d0f6
CoderParth <ghantigalli@gmail.com>
2024-08-18 22:21:39 +1000

    draft: 2015/day16/part1

--- 2015/day16/part1/main.go
+++ 2015/day16/part1/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
 )
@@ -47,43 +48,114 @@ import (
 //
 // What is the number of the Sue that got you the gift?
 
-type Compounds struct {
-	children    string
-	cats        string
-	samoyeds    string
-	pomeranians string
-	akitas      string
-	vizslas     string
-	goldfish    string
-	trees       string
-	cars        string
-	perfumes    string
-}
-
+//	type Compounds struct {
+//		children    string
+//		cats        string
+//		samoyeds    string
+//		pomeranians string
+//		akitas      string
+//		vizslas     string
+//		goldfish    string
+//		trees       string
+//		cars        string
+//		perfumes    string
+//	}
 func main() {
 	detectedCompounds := createDetectedComopoundsMap()
 	fileScanner := createFileScanner()
 	input := parseInput(fileScanner)
+	correctAuntSue := findCorrectSue(detectedCompounds, input)
+	fmt.Printf("Input: %v \n", input)
+	fmt.Printf("Correct Aunt Sue: %v \n", correctAuntSue)
 }
 
-func parseInput(fileScanner *bufio.Scanner) map[string]Compounds {
-}
-
-func createDetectedComopoundsMap() map[string]Compounds {
-	mp := make(map[string]Compounds)
-	c := Compounds{
-		children:    "3",
-		cats:        "7",
-		samoyeds:    "2",
-		pomeranians: "3",
-		akitas:      "0",
-		vizslas:     "0",
-		goldfish:    "5",
-		trees:       "3",
-		cars:        "2",
-		perfumes:    "1",
+func findCorrectSue(c map[string]string, input map[string]map[string]string) string {
+	for currSue, v := range input {
+		numOfSimilarVals := 0
+		for compound, compoundVal := range v {
+			if c[compound] == compoundVal {
+				numOfSimilarVals++
+			}
+		}
+		if numOfSimilarVals == 3 {
+			return currSue
+		}
 	}
-	mp["Real"] = c
+	return "Not Found"
+}
+
+func parseInput(fileScanner *bufio.Scanner) map[string]map[string]string {
+	mp := make(map[string]map[string]string)
+	for fileScanner.Scan() {
+		currLine := fileScanner.Text()
+		n := len(currLine)
+		arr := createArr(currLine, n)          // create array out of data from current line
+		currNum, mpOfThings := createList(arr) // list of things for current Aunt sue
+		mp[currNum] = mpOfThings
+	}
+	return mp
+}
+
+func createList(arr []string) (string, map[string]string) {
+	mp := make(map[string]string)
+	currNum := arr[1]
+	currNum = currNum[0 : len(currNum)-1]
+	first := arr[2]
+	first = first[0 : len(first)-1]
+	firstValue := arr[3]
+	firstValue = firstValue[0 : len(firstValue)-1]
+	second := arr[4]
+	second = second[0 : len(second)-1]
+	secondValue := arr[5]
+	secondValue = secondValue[0 : len(secondValue)-1]
+	third := arr[6]
+	third = third[0 : len(third)-1]
+	thirdValue := arr[7]
+	thirdValue = thirdValue[0 : len(thirdValue)-1]
+	mp[first] = firstValue
+	mp[second] = secondValue
+	mp[third] = thirdValue
+	return currNum, mp
+}
+
+func createArr(line string, n int) []string {
+	arr := []string{}
+	for i := 0; i < n; i++ {
+		if string(line[i]) == " " {
+			continue
+		}
+		curr := ""
+		for j := i; j < n; j++ {
+			if j == n-1 {
+				curr += string(line[j])
+				arr = append(arr, curr)
+				i = j
+				break
+			}
+			if string(line[j]) == " " {
+				arr = append(arr, curr)
+				i = j
+				break
+			}
+			curr += string(line[j])
+		}
+	}
+	return arr
+}
+
+func createDetectedComopoundsMap() map[string]string {
+	mp := map[string]string{
+		"	children": "3",
+		"cats:   ":  "7",
+		"samoyeds":  "2",
+		"pomerani":  "3",
+		"akitas: ":  "0",
+		"vizslas:":  "0",
+		"goldfish":  "5",
+		"trees:  ":  "3",
+		"cars:   ":  "2",
+		"perfumes":  "1",
+	}
 	return mp
 }
 
