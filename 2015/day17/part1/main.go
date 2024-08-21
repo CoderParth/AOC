9a754efb342fdc35b6d58368e6171fc8fc34e1dd
CoderParth <ghantigalli@gmail.com>
2024-08-21 14:42:12 +1000

    draft: 2015/day17/part1

--- 2015/day17/part1/main.go
+++ 2015/day17/part1/main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"bufio"
+	"log"
+	"os"
+	"strconv"
+)
+
 // --- Day 17: No Such Thing as Too Much ---
 // The elves bought too much eggnog again - 150 liters this time.
 // To fit it all into your refrigerator, you'll need to move it
@@ -17,4 +24,44 @@ package main
 // Filling all containers entirely, how many different combinations
 // of containers can exactly fit all 150 liters of eggnog?
 func main() {
+	litersToStore := 25
+	fileScanner := createFileScanner()
+	input := parseInput(fileScanner) // create Array from input
+}
+
+func parseInput(fileScanner *bufio.Scanner) []int {
+	input := []int{}
+	for fileScanner.Scan() {
+		currLine := fileScanner.Text()
+		numAsStr := getCurrNum(currLine)
+		numAsInt := convStrToInt(numAsStr)
+		input = append(input, numAsInt)
+	}
+	return input
+}
+
+func createFileScanner() *bufio.Scanner {
+	readFile, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+	return fileScanner
+}
+
+func getCurrNum(line string) string {
+	n := ""
+	for i := 0; i < len(line); i++ {
+		n += string(line[i])
+	}
+	return n
+}
+
+func convStrToInt(n string) int {
+	num, err := strconv.Atoi(n)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return num
 }
