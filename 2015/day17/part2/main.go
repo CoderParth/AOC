bea3e9345ae50ab806bc1d5a3d7950c8a45e3d6b
CoderParth <ghantigalli@gmail.com>
2024-08-21 21:17:10 +1000

    initial: setup of 2015/day17/part2

---
+++ 2015/day17/part2/main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+// --- Part Two ---
+// While playing with all the containers in the kitchen,
+// another load of eggnog arrives! The shipping and
+// receiving department is requesting as many containers
+// as you can spare.
+//
+// Find the minimum number of containers that can exactly
+// fit all 150 liters of eggnog. How many different ways
+// can you fill that number of containers and still hold
+// exactly 150 litres?
+//
+// In the example above, the minimum number of containers
+// was two. There were three ways to use that many
+// containers, and so the answer there would be 3.
+func main() {
+	litersToStore := 150
+	fileScanner := createFileScanner()
+	input := parseInput(fileScanner) // create Array from input
+	subsets := findSubsets(input)
+	numOfCombinations := findDiffNumOfCombinations(subsets, litersToStore)
+	fmt.Printf("Num of combinations: %v \n", numOfCombinations)
+}
+
+func findDiffNumOfCombinations(subsets [][]int, litersToStore int) int {
+	total := 0
+	for i := 0; i < len(subsets); i++ {
+		sum := calculateSum(subsets[i])
+		if sum == litersToStore {
+			total++
+		}
+	}
+	return total
+}
+
+func calculateSum(nums []int) int {
+	totalSum := 0
+	for i := 0; i < len(nums); i++ {
+		totalSum += nums[i]
+	}
+	return totalSum
+}
+
+func findSubsets(input []int) [][]int {
+	res := [][]int{}
+	curr := []int{}
+	backTrack(0, input, &curr, &res)
+	return res
+}
+
+func backTrack(idx int, input []int, curr *[]int, res *[][]int) {
+	copyOfCurr := make([]int, len(*curr))
+	copy(copyOfCurr, (*curr))
+	*res = append(*res, copyOfCurr)
+
+	for i := idx; i < len(input); i++ {
+		*curr = append(*curr, input[i])
+		backTrack(i+1, input, curr, res)
+		*curr = (*curr)[0 : len(*curr)-1]
+	}
+}
+
+func parseInput(fileScanner *bufio.Scanner) []int {
+	input := []int{}
+	for fileScanner.Scan() {
+		currLine := fileScanner.Text()
+		numAsStr := getCurrNum(currLine)
+		numAsInt := convStrToInt(numAsStr)
+		input = append(input, numAsInt)
+	}
+	return input
+}
+
+func createFileScanner() *bufio.Scanner {
+	readFile, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+	return fileScanner
+}
+
+func getCurrNum(line string) string {
+	n := ""
+	for i := 0; i < len(line); i++ {
+		n += string(line[i])
+	}
+	return n
+}
+
+func convStrToInt(n string) int {
+	num, err := strconv.Atoi(n)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return num
+}
