afc244d6a199f0bf31f9c0eb0ae87fbb7ed8c8ec
CoderParth <ghantigalli@gmail.com>
2024-08-31 13:41:05 +1000

    initial: 2017/day7/part2

---
+++ 2017/day7/part2/main.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+// --- Part Two ---
+// The programs explain the situation: they can't get down. Rather,
+// they could get down, if they weren't expending all of their energy
+// trying to keep the tower balanced. Apparently, one program has the
+// wrong weight, and until it's fixed, they're stuck here.
+//
+// For any program holding a disc, each program standing on that disc
+// forms a sub-tower. Each of those sub-towers are supposed to be the
+// same weight, or the disc itself isn't balanced. The weight of a
+// tower is the sum of the weights of the programs in that tower.
+//
+// In the example above, this means that for ugml's disc to be balanced,
+// gyxo, ebii, and jptl must all have the same weight, and they do: 61.
+//
+// However, for tknk to be balanced, each of the programs standing on
+// its disc and all programs above it must each match. This means that
+// the following sums must all be the same:
+//
+// ugml + (gyxo + ebii + jptl) = 68 + (61 + 61 + 61) = 251
+// padx + (pbga + havc + qoyq) = 45 + (66 + 66 + 66) = 243
+// fwft + (ktlj + cntj + xhth) = 72 + (57 + 57 + 57) = 243
+//
+// As you can see, tknk's disc is unbalanced: ugml's stack is heavier
+// than the other two. Even though the nodes above ugml are balanced,
+// ugml itself is too heavy: it needs to be 8 units lighter for its
+// stack to weigh 243 and keep the towers balanced. If this change
+// were made, its weight would be 60.
+//
+// Given that exactly one program is the wrong weight, what would its
+// weight need to be to balance the entire tower?
+type Disc struct {
+	name   string
+	weight string
+}
+
+func main() {
+	fileScanner := createFileScanner()
+	mp := make(map[string][]string) // map of the discs and their sub-towers
+	for fileScanner.Scan() {
+		disc, subTowers := parseInput(fileScanner.Text())
+		mp[disc] = subTowers
+	}
+	fmt.Printf("Map of discs: %v \n", mp)
+	bottomDisc := findBottomDisc(mp)
+	fmt.Printf("Bottom Disc %v \n", bottomDisc)
+}
+
+func createFileScanner() *bufio.Scanner {
+	readFile, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+	return fileScanner
+}
+
+func parseInput(line string) (string, []string) {
+	n, idx := len(line), 0
+	name := ""
+	// parse name
+	for idx < n {
+		if string(line[idx]) == " " {
+			idx++
+			break
+		}
+		name += string(line[idx])
+		idx++
+	}
+	// parse weight
+	weight := ""
+	idx++
+	for idx < n {
+		if string(line[idx]) == ")" {
+			idx++
+			break
+		}
+		weight += string(line[idx])
+		idx++
+	}
+	// find subTowers
+	idx += 4
+	subTowers := []string{}
+	for idx < n {
+		curr := ""
+		for j := idx; j < n; j++ {
+			if string(line[j]) == "," {
+				subTowers = append(subTowers, curr)
+				idx = j + 2
+				break
+			}
+			if j == n-1 {
+				curr += string(line[j])
+				subTowers = append(subTowers, curr)
+				idx = j + 1
+				break
+			}
+			curr += string(line[j])
+		}
+	}
+	return name, subTowers
+}
+
+func findBottomDisc(mp map[string][]string) string {
+	for disc := range mp {
+		visited := make(map[string]int)
+		searchRecusively(disc, mp, &visited)
+		if len(visited) == len(mp) {
+			return disc
+		}
+	}
+	return "Not found"
+}
+
+func searchRecusively(disc string, mp map[string][]string, visited *map[string]int) {
+	if _, ok := (*visited)[disc]; ok {
+		return
+	}
+	(*visited)[disc] = 0
+	for _, subTower := range mp[disc] {
+		fmt.Printf("Sub Tower: %v \n", subTower)
+		if _, ok := (*visited)[subTower]; !ok {
+			searchRecusively(subTower, mp, visited)
+		}
+	}
+}
