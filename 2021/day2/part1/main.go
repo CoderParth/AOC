e294947b2eadc746d00b9dc0902dac14d736d145
CoderParth <ghantigalli@gmail.com>
2024-09-17 12:23:42 +1000

    draft: 2021/day2/part1

---
+++ 2021/day2/part1/main.go
@@ -0,0 +1,7 @@
+package main
+
+// Calculate the horizontal position and depth you would have after following
+// the planned course. What do you get if you multiply your final
+// horizontal position by your final depth?
+func main() {
+}
